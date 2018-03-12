ae081657ed28b427b349b115bf9ff8cb2069b1dd
sachin Vishwakarma <Sachin.11407940@lpu.in>
2018-03-12 14:37:08 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 #storage credentials 
-storage_account_name = 'tharkidiag639'
-storage_account_key = 'iAFq2IxTxvNEzAVDKcvS2XoMvFW8zNd7hXJulxtleC3XZh8XEhsLPTFPXjxp7n7V2SSTek7ZADklijd7bEcULg=='
-storage_container_name = 'capturedimages'
+storage_account_name = '<storage account name>'
+storage_account_key = '<Connection string>'
+storage_container_name = '< your container name >'
 
 #file data
 local_file_name = 'pp.jpeg'

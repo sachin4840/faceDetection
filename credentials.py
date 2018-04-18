f5d9008866b10eb61f6b86e81c17c944eafca6b1
sachin Vishwakarma <Sachin.11407940@lpu.in>
2018-04-18 09:44:42 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,8 +0,0 @@
-#storage credentials 
-storage_account_name = '<storage account name>'
-storage_account_key = '<Connection string>'
-storage_container_name = '< your container name >'
-
-#file data
-local_file_name = 'pp.jpeg'
-file_content_type = 'image/jpeg'
